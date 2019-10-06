cbd236dfeebbca6ab16cfe732afd3f6e97e6e50e
Lauren Risberg <lauren.risberg@gmail.com>
2019-10-06 14:39:04 -0600

    Refactor - add classes

--- game.rb
+++ game.rb
@@ -26,14 +26,104 @@ if ["yes", "y"].include?(answer)
 
   input = get_answer
 
+  class Thing
+    def initialize(name, actions)
+      @name = name
+      @actions = actions
+    end
+
+    def examine
+      @actions.map do |action|
+        puts "#{action.name} - #{action.description}"
+      end
+    end
+
+    def name
+      @name
+    end
+
+    def actions
+      @actions
+    end
+  end
+
+  class Action
+    def initialize(name, description, outcome)
+      @name = name
+      @description = description
+      @outcome = outcome
+    end
+
+    def name
+      @name
+    end
+
+    def description
+      @description
+    end
+
+    def execute
+      puts @outcome
+    end
+  end
+
+  class Room
+    def initialize(things, description)
+      @things = things
+      @description = description
+    end
+
+    def things
+      @things
+    end
+
+    def description
+      @description
+    end
+  end
+
+  room = Room.new(
+    [
+      Thing.new("cat", [Action.new("pet", "Pet the cat.", "The cat purrs."), Action.new("snuggle", "Snuggle the cat.", "The cat claws at your face. Ow!")]),
+      Thing.new("chair", [Action.new("nudge", "Nudge the chair with your foot.", "You nudge the chair with your foot. It doesn't respond. It's a chair."), Action.new("kick", "Kick the chair hard with your foot!", "You kick the chair. It doesn't respond. It's a chair.")])
+    ],
+    "You find yourself on the floor in a dark room. The floor is wet. Eww. You see a black cat staring at you. There's a chair in the corner."
+  )
+
   while true
     if input == 'examine here'
-      puts "look - Look around the room."
+      puts "look - Look around you"
       puts "examine here - Get a list of commands for what you can do."
       puts "quit - Quit the game."
 
     elsif input == 'look'
-      puts "You find yourself on the floor in a dark room. The floor is wet. Eww."
+      puts room.description
+
+    elsif input.split.length == 2
+      thing_name = input.split[1]
+      action_name = input.split[0]
+
+      thing = room.things.find do |thing|
+        thing.name == thing_name
+      end
+
+      if thing
+        if action_name == "examine"
+          thing.examine
+        else
+          action = thing.actions.find do |action|
+            action.name == action_name
+          end
+
+          if action
+            action.execute
+          else
+            puts "Unrecognized command. Try 'examine here'"
+          end
+        end
+      else
+        puts "Unrecognized command. Try 'examine here'"
+      end
 
     elsif input == 'quit'
       puts "Bye."
@@ -43,6 +133,7 @@ if ["yes", "y"].include?(answer)
       puts "Unrecognized command. Try 'examine here'"
 
     end
+
     input = get_answer
   end
 
