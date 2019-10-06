dc14e675e82183d10643bf282fbb2e0a1116acc3
Lauren Risberg <lauren.risberg@gmail.com>
2019-10-06 13:03:34 -0600

    Ask for Character name

--- game.rb
+++ game.rb
@@ -3,12 +3,22 @@ def get_answer
 end
 
 
-puts 'Would you like to play this game? (yes/no)'
+puts "Would you like to play this game? (yes/no)"
 
 answer = get_answer
 
-if answer == 'yes'
-  puts 'Hooray!'
+if answer == "yes"
+  puts "Enter your character's name."
+
+  name = get_answer
+
+  while name == ""
+    puts "Name cannot be blank. Try again."
+
+    name = get_answer
+  end
+
+  puts "Your name is #{name}."
 else
-  puts 'booo'
+  puts "Bye."
 end
